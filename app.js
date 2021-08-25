ac7dd6b6b321265affa87cebbff7f01a19577b65
Aayush2306 <64463251+Aayush2306@users.noreply.github.com>
2021-08-25 14:16:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-const apiKey1 = "18a3d559441d02bb3a4b8e339614ac1f";
-const apiKey2 = "643fafec9f74cdc48bfe5afa6a584105";
-
+const apiKey1 = "77fff2758bbb9112c8492cd21a712a19";
 const form = document.querySelector("form");
 const weatherDetails = document.querySelector(".details");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const location = document.querySelector("input").value;
-  getWeather(location);
+  const locaton = document.querySelector("input").value;
+  getWeather(locaton);
 });
 
-async function getWeather(location) {
-  const response = await fetch(
-    `http://api.weatherstack.com/current?access_key=${apiKey1}&query=${location}`
+async function getWeather(locaton) {
+  const res = await fetch(
+    `http://api.weatherstack.com/current?access_key=${apiKey1}&query=${locaton}`
   );
-  const responseData = await response.json();
-  displayWeather(responseData);
+  const data = await res.json();
+  console.log(data);
+  displayWeather(data);
 }
 
-function displayWeather(weather) {
-  let currentWeather = weather.current;
-  let locationWeather = weather.location;
-
-  const weatherEl = `
+function displayWeather(data) {
+  let markup = "";
+  let current = data.current;
+  let locaton = data.location;
+  let request = data.request;
+  markup = `
   <div class="weather-info">
   <div class="titles">
-    <h2 class="temp">${currentWeather.temperature}<sup>&#8451</sup></h2>
-    <h2 class="status">${currentWeather.weather_descriptions}</h2>
+    <h2 class="temp">${current.temperature}<sup>&#8451</sup></h2>
+    <h2 class="status">${current.weather_descriptions}</h2>
   </div>
 
   <div class="extra-info">
-    <p>Temperature: <span>${currentWeather.temperature}<sup>&#8451</sup></span>  </p>
-    <p>Feels Like: <span>${currentWeather.feelslike}<sup>&#8451</sup></span>  </p>
-    <p>Local Time: <span>${locationWeather.localtime}</span>  </p>
-    <p>Precipitation: <span>${currentWeather.precip}mm</span> </p>
-    <p>Humidity: <span>${currentWeather.humidity}%</span>  </p>
-    <p>Pressure: <span>${currentWeather.pressure}hPa</span>  </p>
-    <p>Cloud Cover: <span>${currentWeather.cloudcover}%</span>  </p>
-    <p>Wind Direction: <span>${currentWeather.wind_dir}</span>  </p>
-    <p>Wind Degree: <span>${currentWeather.wind_degree}</span> </p>
-    <p>Wind Speed: <span>${currentWeather.wind_speed}km/hr</span>  </p>
-    <p>Latitude:  <span>${locationWeather.lat}</span> </p>
-    <p>Longitude:  <span>${locationWeather.lon}</span> </p>
-    <p>Visibility: <span> ${currentWeather.visibility}km</span> </p>
-    <p>Region: <span>${locationWeather.region}</span>  </p>
+    <p>Temperature: <span>${current.temperature}<sup>&#8451</sup></span>  </p>
+    <p>Feels Like: <span>${current.feelslike}<sup>&#8451</sup></span>  </p>
+    <p>Local Time: <span>${locaton.localtime}</span>  </p>
+    <p>Precipitation: <span>${current.precip}mm</span> </p>
+    <p>Humidity: <span>${current.humidity}%</span>  </p>
+    <p>Pressure: <span>${current.pressure}hPa</span>  </p>
+    <p>Cloud Cover: <span>${current.cloudcover}%</span>  </p>
+    <p>Wind Direction: <span>${current.wind_dir}</span>  </p>
+    <p>Wind Degree: <span>${current.wind_degree}</span> </p>
+    <p>Wind Speed: <span>${current.wind_speed}km/hr</span>  </p>
+    <p>Latitude:  <span>${locaton.lat}</span> </p>
+    <p>Longitude:  <span>${locaton.lon}</span> </p>
+    <p>Visibility: <span> ${current.visibility}km</span> </p>
+    <p>Region: <span>${locaton.region}</span>  </p>
   </div>
  </div>
 
- <div class="place">${locationWeather.name}, ${locationWeather.country}</div>
+ <div class="place">${locaton.name}, ${locaton.country}</div>
   `;
 
-  weatherDetails.innerHTML = weatherEl;
+  weatherDetails.innerHTML = markup;
 }
